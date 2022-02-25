ba6d327a5cd3394b1e1462ab6b8640dc6b6651e8
Scott Trent <trent@jp.ibm.com>
2022-02-24 19:54:38 -0600

    remove synset code

--- src/main/java/com/ibm/trl/funqy/inference/Param.java
+++ src/main/java/com/ibm/trl/funqy/inference/Param.java
@@ -4,7 +4,7 @@ package com.ibm.trl.funqy.inference;
 public class Param {
     private String input;
     private String model;
-    private String synset;
+//    private String synset;
 
     public Param() {}
 
@@ -14,6 +14,6 @@ public class Param {
     public String getModel() { return model; }
     public void setModel(String m) { this.model = m; }
 
-    public String getSynset() { return synset; }
-    public void setSynset(String s) { this.synset = s; }
+//    public String getSynset() { return synset; }
+//    public void setSynset(String s) { this.synset = s; }
 }
