5d90e3a93cb052fe107f8adad83f827a153ecf5f
Michihiro Horie <horie@jp.ibm.com>
2022-03-07 17:06:33 +0900

    Add VideoProcessing benchmark

---
+++ benchmarks/videoprocessing/README.md
@@ -0,0 +1,72 @@
+# multimedia-funqy Project
+
+This is a project to port and test [serverless-benchmarks](https://github.com/spcl/serverless-benchmarks) using Quarkus
+[Funqy HTTP Binding](https://quarkus.io/guides/funqy-http), which creates a stand-alone application using serverless functions.
+This interim step should be useful to port the benchmark suite to knative environment using [Quarkus Funqy](https://quarkus.io/guides/funqy).
+This project is also useful to verify if there is any problems to build into native images.
+
+Video-processing benchmark uses [FFmpeg Java](https://github.com/bramp/ffmpeg-cli-wrapper).
+
+If you want to learn more about Quarkus, please visit its website: https://quarkus.io/ .
+
+## Packaging and running the application
+
+The application can be packaged using:
+```shell script
+$ ./mvnw clean package
+```
+It produces the `quarkus-run.jar` file in the `target/quarkus-app/` directory.
+Be aware that it’s not an _über-jar_ as the dependencies are copied into the `target/quarkus-app/lib/` directory.
+
+The application is now runnable using:
+```shell script
+$ java -jar target/quarkus-app/quarkus-run.jar
+```
+
+Now the server listens to `localhost:8080`, and functions are accessible at `/<functionName>` path. 
+The functions taking parameters only accespt POST request. The functions taking no parameter accept both GET and POST request.
+
+The `/pagerank` function receives a test data size as a string, and returns result in JSON format:
+```
+$ curl -s -w "\n" -H 'Content-Type:application/json' -d '{"key": "Anthem-30-16x9-lowres.mp4", "height": "128", "width": "128", "duration": "1", "op": "extract-gif"}' -X POST http://localhost:8080/videoprocessing jq
+{
+  "result": {
+    "key": "processed-Anthem-30-16x9-lowres.gif",
+    "bucket": "trl-knative-benchmark-bucket-2"
+  },
+  "measurement": {
+    "download_time": 9790609,
+    "compute_time": 2318449,
+    "upload_size": 1177929,
+    "upload_time": 2188424,
+    "download_size": 1391747
+  }
+}
+```
+
+Be careful about quartation marks. In Funqy, post data need to be JSON format. So, a string value in post data needs to have double quatation marks (`"`)
+aroud the data. You may also need another quartation marks or back-quote (`\`) to avoid shell command line interpretation.
+
+### Building an über-jar
+If you want to build an _über-jar_, execute the following command:
+```shell script
+./mvnw package -Dquarkus.package.type=uber-jar
+```
+
+The application, packaged as an _über-jar_, is now runnable using `java -jar target/*-runner.jar`.
+
+## Creating a native executable
+
+You can create a native executable using: 
+```shell script
+./mvnw package -Pnative
+```
+
+Or, if you don't have GraalVM installed, you can run the native executable build in a container using: 
+```shell script
+./mvnw package -Pnative -Dquarkus.native.container-build=true
+```
+
+You can then execute your native executable with: `./target/knative-serverless-benchmark-videoprocessing-1.0.0-SNAPSHOT.jar`
+
+If you want to learn more about building native executables, please consult https://quarkus.io/guides/maven-tooling.html.
---
+++ benchmarks/videoprocessing/src/main/java/com/ibm/trl/serverlessbench/VideoProcessing.java
@@ -0,0 +1,162 @@
+package com.ibm.trl.serverlessbench;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.BiFunction;
+import java.util.HashMap;
+import java.util.Map;
+import java.nio.file.Files;
+
+import io.quarkus.funqy.Funq;
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+
+import javax.inject.Inject;
+
+import software.amazon.awssdk.services.s3.S3Client;
+
+public class VideoProcessing {
+    private COSUtils client;
+
+    @Inject
+    S3Client s3;
+
+    public VideoProcessing() throws Exception {
+        client = new COSUtils();
+    }
+    
+    private void call_ffmpeg(String[] args) {
+    }
+
+    BiFunction<String, Integer, String> to_gif = (video, duration) -> {
+        String output = String.format("/tmp/processed-%s.gif", video.substring(video.lastIndexOf('/')+1, video.lastIndexOf('.')));
+        FFmpeg ffmpeg = null;
+        FFprobe ffprobe = null;
+        try {
+            ffmpeg = new FFmpeg("/usr/bin/ffmpeg");
+            ffprobe = new FFprobe("/usr/bin/ffprobe");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        FFmpegBuilder builder = new FFmpegBuilder()
+                .setInput(video)
+                .overrideOutputFiles(true)
+                .addOutput(output)
+                .setFormat("mp4")
+                .done();
+
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);        
+        executor.createJob(builder).run();
+
+        return output;
+    };
+
+    BiFunction<String, Integer, String> watermark = (video, duration) -> {
+        String output = String.format("/tmp/processed-%s.gif", video.substring(video.lastIndexOf('/')+1, video.lastIndexOf('.')));
+        String watermark_file = "src/main/resources/watermark.png";
+        FFmpeg ffmpeg = null;
+        FFprobe ffprobe = null;
+        try {
+            ffmpeg = new FFmpeg("/usr/bin/ffmpeg");
+            ffprobe = new FFprobe("/usr/bin/ffprobe");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        FFmpegBuilder builder = new FFmpegBuilder()
+                .setInput(video)
+                .addInput(watermark_file)
+                .overrideOutputFiles(true)
+                .addOutput(output)
+                .setFormat("mp4")
+                .done();
+
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+        executor.createJob(builder).run();
+
+        return output;
+    };
+
+    BiFunction<String, Integer, String> transcode_mp3 = (video, duration) -> { return null; };
+
+    private Map<String, BiFunction<String, Integer, String>> operations = Map.of("transcode", transcode_mp3,
+                                                                                 "extract-gif", to_gif,
+                                                                                 "watermark", watermark);
+    
+    @Funq
+    public RetVal videoprocessing(Param param) throws Exception {
+        String input_bucket = client.getInBucket();
+        String output_bucket = client.getOutBucket();
+        String key = param.getKey();
+        int duration = param.getDuration();
+        String op = param.getOp();
+        String download_path = String.format("/tmp/%s", key);
+
+        long download_begin = System.nanoTime();
+        download(input_bucket, key, download_path);
+        long download_size = Files.size(new File(download_path).toPath());
+        long download_stop = System.nanoTime();
+
+        long process_begin = System.nanoTime();
+        String upload_path = operations.get(op).apply(download_path, duration);
+        long process_end = System.nanoTime();
+
+        long upload_begin = System.nanoTime();
+        File f1 = new File(upload_path);
+        File f = new File(key);
+        String out_key = ((f.getParent() != null)? f.getParent() + "/" : "") + f1.getName();
+        long upload_size = Files.size(new File(upload_path).toPath());
+        upload(output_bucket, out_key, upload_path);
+        long upload_stop = System.nanoTime();
+
+        long download_time = (download_stop - download_begin)/1000;
+        long upload_time = (upload_stop - upload_begin)/1000;
+        long process_time = (process_end - process_begin)/1000;
+
+        RetVal retVal = new RetVal();
+        retVal.result = Map.of(     "bucket", output_bucket,
+                                    "key", out_key);
+        retVal.measurement = Map.of("download_time", download_time,
+                                    "download_size", download_size,
+                                    "upload_time", upload_time,
+                                    "upload_size", upload_size,
+                                    "compute_time", process_time);
+        return retVal;
+    }
+    
+    private void upload(String output_bucket, String filename, String upload_path) throws Exception {        
+        this.client.uploadFile(s3, output_bucket, filename, upload_path);
+    }
+
+    private void download(String input_bucket, String key, String download_path) throws Exception {
+        this.client.downloadFile(s3, input_bucket, key, download_path);
+    }
+
+    public static class RetVal {
+        Map<String, String> result;
+        Map<String, Long> measurement;
+
+        public Map<String, String> getResult() {
+            return result;
+        }
+
+        public void setResult(Map<String, String> result) {
+            this.result = result;
+        }
+
+        public Map<String, Long> getMeasurement() {
+            return measurement;
+        }
+
+        public void setMeasurement(Map<String, Long> measurement) {
+            this.measurement = measurement;
+        }
+
+        RetVal() {
+            measurement = new HashMap<String, Long>();
+        }
+    }
+}
